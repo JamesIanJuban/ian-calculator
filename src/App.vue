16e3bbe9f389de128da337938161695c4c87b9f8
James Ian Juban <jamesiansama0924@gmail.com>
2024-05-07 16:46:33 +0800

    Funtional

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <article class="text-center"> 
-    <h1 class="text-4xl text-white bg-gradient-to-br from-gray-800 to-gray-900 mt-8 font-bold">Scientific Calculator</h1>
+    <h1 class="text-4xl text-white bg-gradient-to-br from-gray-800 to-gray-900 mt-10 font-bold">Scientific Calculator</h1>
       <CalculatorContainer/>
   </article>
 </template>
