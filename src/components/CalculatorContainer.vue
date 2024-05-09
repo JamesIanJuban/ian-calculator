86842a4c9b971b586390e5b7346e2e4a340aca58
James Ian Juban <jamesiansama0924@gmail.com>
2024-05-09 11:26:57 +0800

    link history

--- src/components/CalculatorContainer.vue
+++ src/components/CalculatorContainer.vue
@@ -1,44 +1,64 @@
 <template>
-    <!-- Container with gradient background -->
-    <div class="container justify-center items-center p-8 relative bg-gradient-to-br from-gray-800 to-gray-900">
+  <!-- Container with gradient background -->
+  <div class="container justify-center items-center p-8 relative bg-gradient-to-br from-gray-800 to-gray-900">
     <!-- Calculator display -->
     <div class="display mb-6 mt-10 flex justify-center">
-        <input type="text" v-model="inputStr" placeholder="Enter Expression" @keydown.enter="calculate" @input="validateInput" class="input-text w-full max-w-xl px-6 py-4 md:py-6 text-lg rounded-lg text-white border-2 border-red-500/50 bg-red-600 shadow-lg shadow-red-500/50 dark:shadow-lg dark:shadow-red-800/80 placeholder-gray-100 focus:border-red-500 focus:outline-none transition duration-300 ease-in-out">
+      <input type="text" v-model="inputStr" placeholder="Enter Expression" @keydown.enter="calculate" @input="validateInput" class="input-text w-full max-w-xl px-6 py-4 md:py-6 text-lg rounded-lg text-white border-2 border-red-500/50 bg-red-600 shadow-lg shadow-red-500/50 dark:shadow-lg dark:shadow-red-800/80 placeholder-gray-100 focus:border-red-500 focus:outline-none transition duration-300 ease-in-out">
     </div>    
     <!-- Operation buttons -->  
     <div class="grid grid-cols-5 gap-4 px-6 py-4">
-        <button v-for="item in numeric" :key="item.text" class="text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 shadow-lg shadow-cyan-500/50 dark:shadow-lg dark:shadow-cyan-800/80 font-medium rounded-lg px-6 py-3 text-center mr-1 mb-2" @click="handleButtonClick(item)">{{ item.text }}</button>  
-        <button class="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 shadow-lg shadow-red-500/50 dark:shadow-lg dark:shadow-red-800/80 font-medium rounded-lg text-sm px-6 py-2.5 text-center mr-1 mb-2" @click="remove">←</button>
-        <button class="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 shadow-lg shadow-red-500/50 dark:shadow-lg dark:shadow-red-800/80 font-medium rounded-lg text-sm px-6 py-2.5 text-center mr-1 mb-2" @click="clear">AC</button>
-        <button class="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 shadow-lg shadow-red-500/50 dark:shadow-lg dark:shadow-red-800/80 font-medium rounded-lg text-sm px-6 py-2.5 text-center mr-1 mb-2" @click="calculate">=</button>
+      <button v-for="item in numeric" :key="item.text" class="text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 shadow-lg shadow-cyan-500/50 dark:shadow-lg dark:shadow-cyan-800/80 font-medium rounded-lg px-6 py-3 text-center mr-1 mb-2" @click="handleButtonClick(item)">{{ item.text }}</button>  
+      <button class="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 shadow-lg shadow-red-500/50 dark:shadow-lg dark:shadow-red-800/80 font-medium rounded-lg text-sm px-6 py-2.5 text-center mr-1 mb-2" @click="remove">←</button>
+      <button class="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 shadow-lg shadow-red-500/50 dark:shadow-lg dark:shadow-red-800/80 font-medium rounded-lg text-sm px-6 py-2.5 text-center mr-1 mb-2" @click="clear">AC</button>
+      <button class="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 shadow-lg shadow-red-500/50 dark:shadow-lg dark:shadow-red-800/80 font-medium rounded-lg text-sm px-6 py-2.5 text-center mr-1 mb-2" @click="calculate">=</button>
     </div>
     <!-- Menu bar for additional buttons -->
     <div class="flex justify-end items-start mr-8 mt-4 mb-6">
-        <button class="text-white font-medium rounded-lg py-2 px-4 md:py-4 text-lg bg-gradient-to-r from-red-400 to-red-600 hover:from-red-500 hover:to-red-700 focus:ring-4 focus:ring-red-300 focus:outline-none shadow-md transition duration-300 ease-in-out" @click="addSpace">Space</button>
+      <button class="text-white font-medium rounded-lg py-2 px-4 md:py-4 text-lg bg-gradient-to-r from-red-400 to-red-600 hover:from-red-500 hover:to-red-700 focus:ring-4 focus:ring-red-300 focus:outline-none shadow-md transition duration-300 ease-in-out" @click="addSpace">Space</button>
     </div>
-        <!-- Calculation History Menu -->
-        <button @click="toggleHistory" class="text-white font-medium rounded-lg py-2 px-4 md:py-4 text-lg bg-gradient-to-r from-red-400 to-red-600 hover:from-red-500 hover:to-red-700 focus:ring-4 focus:ring-red-300 focus:outline-none shadow-md transition duration-300 ease-in-out text-center mr-1 mb-2 bg-gradient-to-br from-gray-800 to-gray-900"> {{ showHistory ? 'Hide History' : 'Show History' }} </button>
-     <!-- Calculation History Menu -->
-     <div v-if="showHistory" class="absolute left-1/2 transform -translate-x-1/2 w-full h-40 overflow-y-auto bg-gradient-to-br from-gray-800 to-gray-900 text-white shadow-lg z-10 mt-6">
-      <h3 class="text-white px-6 py-3 font-semibold bg-gradient-to-r from-red-400 via-red-500 to-red-600 transition-colors duration-300 hover:bg-red-700"> History </h3> 
-      <ul class="divide-y divide-red-700"><li v-for="(calculation, index) in history.slice().reverse()" :key="index" class="px-4 py-3 transition duration-300 ease-in-out hover:bg-gradient-to-br hover:from-gray-900 hover:to-gray-800">{{ calculation }}</li></ul>
+    <!-- Calculation History Menu -->
+    <button @click="toggleHistory" class="text-white font-medium rounded-lg py-2 px-4 md:py-4 text-lg bg-gradient-to-r from-red-400 to-red-600 hover:from-red-500 hover:to-red-700 focus:ring-4 focus:ring-red-300 focus:outline-none shadow-md transition duration-300 ease-in-out text-center mr-1 mb-2 bg-gradient-to-br from-gray-800 to-gray-900"> {{ showHistory ? 'Hide History' : 'Show History' }} </button>
+  </div>
+
+ <!-- History container -->
+ <transition name="slide">
+    <div v-if="showHistory" class="history-container fixed right-0 top-0 h-screen w-1/2 bg-gray-900 shadow-lg overflow-y-auto z-50">
+      <!-- Close button -->
+    <button @click="toggleHistory" class="close-button absolute top-4 right-4 bg-red-500 text-white font-bold rounded-full h-8 w-8 flex items-center justify-center focus:outline-none">
+      <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"/>
+      </svg>
+    </button>
+      <HistoryWindow :show="showHistory" :history="history" @toggle="toggleHistory" />
+      <ul class="divide-y divide-red-700">
+        <li v-for="(calculation, index) in history.slice().reverse()" :key="index" class="px-2 py-1 hover:bg-gray-700">
+          <div class="flex justify-between items-center">
+            <span class="text-white">{{ calculation.expression }}</span>
+            <span class="text-green-400">{{ calculation.result }}</span>
+          </div>
+        </li>
+      </ul>
     </div>
-    </div>
+  </transition>
 </template>
 
 <script>
+import HistoryWindow from './HistoryWindow.vue';
 import { numeric } from '@/data.js';
 
 export default {
   name: "CalculatorContainer",
-    data() {
-      return {
-            inputStr: '',
-            numeric: numeric,
-            history: [],
-            showHistory: false
-      };
-    },
+  components: {
+    HistoryWindow
+  },
+  data() {
+    return {
+      inputStr: '',
+      numeric: numeric,
+      history: [],
+      showHistory: false
+    };
+  },
   methods: {
     handleButtonClick(button) {
       if (button.operation) {
---
+++ src/components/HistoryWindow.vue
@@ -0,0 +1,27 @@
+<template>
+    <transition name="slide-fade">
+      <div v-if="show" class="absolute top-0 left-0 w-full h-full flex justify-center items-center z-50 bg-black bg-opacity-50">
+        <div class="bg-white rounded-lg p-8 max-w-lg">
+          <h3 class="text-lg font-semibold mb-4">History</h3>
+          <ul class="divide-y divide-gray-300">
+            <li v-for="(calculation, index) in history.slice().reverse()" :key="index" class="py-2">{{ calculation }}</li>
+          </ul>
+          <button @click="toggle" class="mt-4 bg-red-500 hover:bg-red-600 text-white font-semibold px-4 py-2 rounded-lg focus:outline-none">Close</button>
+        </div>
+      </div>
+    </transition>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      show: Boolean,
+      history: Array
+    },
+    methods: {
+      toggle() {
+        this.$emit('toggle');
+      }
+    }
+  };
+  </script>
